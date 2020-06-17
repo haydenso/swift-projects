d0021c4373ce8695a7ebdada0d2299430435da30
haydenso <sohayden929@gmail.com>
2020-06-18 00:07:11 +0800

    test

--- helloworld.playground/Contents.swift
+++ helloworld.playground/Contents.swift
@@ -1,5 +1,36 @@
 import UIKit
 
-if let str = readLine() {
-   print(str)
+var character:Character = "d"
+// variable character of type Character (single character)
+
+
+if character == "d"{
+    print(character)
 }
+
+// like python if else statement
+// comma (,) is like or in python
+switch character{
+    
+    case "a":
+        print("it is an a")
+    
+    case "b", "c":
+        print("it is a B", character)
+    
+    default:
+        print("else")
+}
+
+var sum = 0
+
+//for loop (... is the range part of the for loop)
+for i in 1...5{
+    
+    sum += 1
+    print(sum)
+    print("round", i)
+}
+
+// while loop
+
---
+++ testapp/testapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testapp
+//
+//  Created by HaydSo on 25/5/2020.
+//  Copyright © 2020 Hayden So. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
